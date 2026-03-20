edf9e7bc3cd42a923066128e5198557617cbe5b3
キヨミ <kiyomikoide1111@gmail.com>
2026-03-20 23:55:04 +0900

    hardcode asset url

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_ASSET_URL: "https://studymetervideos.s3.ap-northeast-1.amazonaws.com/entity",
+  },
 };
 
 export default nextConfig;
